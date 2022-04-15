a32598fe82bec518490138a2535fb9c3193e4fb8
Deygerson-Mndz <74508196+Deygerson-Mndz@users.noreply.github.com>
2022-04-15 12:40:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,18 +27,23 @@ fruit_selected = streamlit.multiselect("Pick some fruit:", list(fruits_list.inde
 fruit_to_show = fruits_list.loc[fruit_selected]
 streamlit.dataframe(fruit_to_show)
 
+#Add function for repeatable code block
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_response_normalized = pd.json_normalize(fruityvice_response.json())
+  return fruityvice_response_normalized
+
 #Header 
 streamlit.header('Fruityvice Fruit Advice!')
 #New section to display API response
 try:
-  fruit_choise = streamlit.text_input('What Fruit would you like information about?', 'banana')
+  fruit_choise = streamlit.text_input('What Fruit would you like information about?')
   #streamlit.write('The user entered', fruit_choise)
   if not fruit_choise:
     streamlit.error('Please select a fruit to get information')
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choise)
-    fruityvice_response_normalized = pd.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_response_normalized )
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 
 except URLError as e:
   streamlit.error()
